8829a67046cbee1b0432c65e4f8b737784256d48
Cole Macdonald <colesinbox@gmail.com>
2017-02-09 20:49:58 -0800

    small

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #sws test shell script
 
-echo -e -n "GET / HTTP/1.0\r\n\r\n" |  nc -u -s 192.168.1.100 10.10.1.100 8080 &
-echo -e -n "GET / HTTP/1.0\r\n\r\n" |  nc -u -s 192.168.1.100 10.10.1.100 8080 &
-echo -e -n "GET / HTTP/1.0\r\n\r\n" |  nc -u -s 192.168.1.100 10.10.1.100 8080
+echo -e -n "GET / HTTP/1.0\r\n\r\n" |  nc -u -s 192.168.1.100 10.10.1.100 3080 &
+echo -e -n "GET /nofile HTTP/1.0\r\n\r\n" |  nc -u -s 192.168.1.100 10.10.1.100 3080 &
+echo -e -n "GET / HTTP/1.1\r\n\r\n" |  nc -u -s 192.168.1.100 10.10.1.100 3080
