a1dbece0fc9d39bc51ffcf1237b21ea014e3fb19
Cole Macdonald <colesinbox@gmail.com>
2017-02-10 08:58:43 -0800

    enabled setsockopt

--- sws_server.c
+++ sws_server.c
@@ -183,7 +183,7 @@ int prepareSocket()
 	}
 
 	//http://stackoverflow.com/questions/24194961/how-do-i-use-setsockoptso-reuseaddr
-	//setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 
 	//struct sockaddr_in sa; 
 	char buffer[1024];
